fce63b1ab8934cb03f0f6ea6078c2f6a4a66332b
BigRedS <ialoneambest@gmail.com>
2011-09-04 23:54:56 +0100

    Now uses Mail::vpopmail which is actually maintained and working, if a little odd

--- export.pl
+++ export.pl
@@ -5,69 +5,45 @@
 # Exports a dump of the (interesting bits) of the current vpopmail
 # configuration suitable for shoving into import.pl/.
 #
-##Todo: Handle aliases. Oops
-##Todo: Be able to run on a remote server by doing things via SSH.
 
 use strict;
 
-use vpopmail;
+#use vpopmail;
+use Mail::vpopmail;
 use Data::Dumper;
 $Data::Dumper::Purity = 1;
 
 my %data;
 
-foreach my $domain (vlistdomains()) {
-#my $domain = 'coolcars4hire.co.uk';
-	foreach my $user (vlistusers($domain)){
-		my $username = $user->{pw_name}."@".$domain;
-		my $clearTextPassword = &getClearTextPassword($username);
-		$data{$username}{username}=$username;
-		$data{$username}{password}=$clearTextPassword;
-		$data{$username}{directory}=$user->{pw_dir};
-		my @dotQmail = getDotQmailFile($user->{pw_dir});
-		my @forwards; # = grep( /^\&(\S+)/, @dotQmail);
-		foreach(@dotQmail){
-			if (/^\|\s+\S+autorespond\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/){
-				my %autorespond = (
-					time => $1,
-					count => $2,
-					message => getVacationMessage($3),
-					directory => $4,
-				);
-				$data{$username}{autorespond}=\%autorespond;
-			}elsif(/^\&(\S+)/){
-				push(@forwards,$1);
-			}
+my $vpopmail = Mail::vpopmail->new(debug=>'0');
+
+my %data;
+my @domains = @{$vpopmail->alldomains(field => 'name')};
+foreach my $domain (@domains){
+	my @mailboxes = @{$vpopmail->domaininfo(domain => $domain, field=>'mailboxes')};
+	foreach my $user (@mailboxes){
+		my $email = $user.'@'.$domain;
+		my @forwardto;
+		#print $email."  ";
+		$data{$domain}{$email}{'dir'}     = $vpopmail->userinfo(email=>$email, field=>'dir');
+		$data{$domain}{$email}{'plain'}   = $vpopmail->userinfo(email=>$email, field=>'plain');
+		$data{$domain}{$email}{'comment'} = $vpopmail->userinfo(email=>$email, field=>'comment');
+		$data{$domain}{$email}{'quota'}   = $vpopmail->userinfo(email=>$email, field=>'quota');
+		my @dotQmailFile = getDotQmailFile($data{$domain}{$email}{'dir'}, $user);
+		foreach(@dotQmailFile){
+			if (/^\s*\&(.+)/){push(@forwardto, $1);}
 		}
-
-
-		$data{$username}{forwards} = \@forwards;
-		$data{$username}{dotqmail} = \@dotQmail;
+		$data{$domain}{$email}{'forwardto'} = \@forwardto;
+		$data{$domain}{$email}{'dotqmail'} = \@dotQmailFile;
 	}
 }
+print Dumper(%data);
 
-print Dumper(\%data);
-
-
-sub getVacationMessage{
-	my $file = shift;
-	if (-f $file){
-		my $f;
-		eval{
-			open($f, "<", $file) or die "Error opening vacation message at $file for reading";
-		};
-		print $@;
-		return if ($@);
-		my @file = (<$f>);
-		my $message = join(/\n/, @file);
-		return $message;
-	}
-	return;
-}
 
 sub getDotQmailFile{
 	my $dir = shift;
-	my $file = $dir."/.qmail";
+	my $user = shift;
+	my $file = $dir."/.qmail-$user";
 	if(-f $file){
 		my ($f,@file);
 		eval{ 
@@ -80,18 +56,6 @@ sub getDotQmailFile{
 		}
 		close($f);
 		return @file;
-	}
-	return;
-}
-
-sub getClearTextPassword{
-	my $user = shift;
-	my $password;
-	foreach(`vuserinfo $user`){
-		if (/clear passwd: (\S+)/){
-			return $1;
-			last;
-		}
 	}
 	return;
 }
