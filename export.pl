21261d8156a44a331e5714ff7cff06d2ec4b2523
BigRedS <ialoneambest@gmail.com>
2011-09-17 10:56:25 +0100

    Now copes with aliases, and offers help

--- export.pl
+++ export.pl
@@ -12,14 +12,15 @@ use strict;
 use Mail::vpopmail;
 use Data::Dumper;
 $Data::Dumper::Purity = 1;
-
-my %data;
+$Data::Dumper::Useqq = 1;
 
 my $vpopmail = Mail::vpopmail->new(debug=>'0');
 
+my $file = $ARGV[0] || usage();
+
 my %data;
-my @domains = @{$vpopmail->alldomains(field => 'name')};
-foreach my $domain (@domains){
+my %data = getDomains();
+foreach my $domain (keys(%data)){
 	my @mailboxes = @{$vpopmail->domaininfo(domain => $domain, field=>'mailboxes')};
 	foreach my $user (@mailboxes){
 		my $email = $user.'@'.$domain;
@@ -37,9 +38,28 @@ foreach my $domain (@domains){
 		$data{$domain}{$email}{'dotqmail'} = \@dotQmailFile;
 	}
 }
-print Dumper(%data);
+print Dumper(\%data);
 
+# Passed nothing, returns a hash whose keys are domain names, and each
+# domain has a 'name' element containing its name (equiv. to the key)
+# and, if there are any, an 'aliases' element, containing an array of
+# domains which are aliases to this one.
+sub getDomains{
+	my %domains;
+	foreach(qx/vdominfo -n/){
+		/^(\S+)/;
+		my $domain = $1;
+		if (/\(alias of (.+)\)/){
+			my $alias = $1;
+			push(@{$domains{$domain}{'aliases'}}, $1);
+		}
+		$domains{$domain}{'name'}='$domain';
+	}
+	return %domains;
+}
 
+# Passed a (domain) directory and a user, retrieves that user's .qmail file,
+# and returns its contents as a one-line-per-element array
 sub getDotQmailFile{
 	my $dir = shift;
 	my $user = shift;
@@ -58,4 +78,25 @@ sub getDotQmailFile{
 		return @file;
 	}
 	return;
+}
+
+sub usage(){
+
+print <<EOF;
+	export.pl
+
+	part of vpopmail2postfixadmin
+
+usage:
+
+	export.pl <filename>
+
+dumps the configuration of a vpopmail system to 
+<filename>, in a format understood by import.pl, 
+which will import it to a postfixadmin system.
+
+EOF
+
+exit 1;
+
 }
